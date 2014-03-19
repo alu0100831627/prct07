3b96f451d1ecc5e3f88d75a8679eedc2fba54ce1
Raquel Espino Manatas <alu0100831627@ull.edu.es>
2014-03-19 14:05:31 +0000

    Ejercicio casi terminado

---
+++ uso_modulopi.py
@@ -0,0 +1,20 @@
+#! usr/bin/python
+#! encoding UTF-8
+def imprimir(l):
+  print "i \ t_upla1 \ t_upla2 \ t_upla3 \ t_ upla4 \ t_upla5 \ t_upla6"
+  
+  for
+
+import modulopi
+t_upla=(10,100,1000,10000,100000,1000000)
+l=[ ]
+for i in t_upla:
+  x=modulopi.aproxpi(i)
+  l=l+[x] 
+  print x
+print l
+
+# el numero maximo de t_upla es 6
+# se producen errores para 8 elementos
+# los elementos de t_upla si se pueden escribir en notacion cientifica
+#la extención .pyc es una version compilada de un modulo ya interpretado
